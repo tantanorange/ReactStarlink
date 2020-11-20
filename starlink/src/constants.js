593aeb1d3a6f69b1592b1ba88b4e743b269ff2a5
tantanorange <44329336+tantanorange@users.noreply.github.com>
2020-11-20 18:44:24 -0500

    Use axios proxy to fetch data from satellite api. Feed data to satellite list node.

--- starlink/src/constants.js
+++ starlink/src/constants.js
@@ -1,3 +1,3 @@
-export const SAT_API_KEY = "R2ZQHW-DTFAJL-ZZN6NM-4KZE";
+export const SAT_API_KEY = process.env.SAT_API_KEY;
 export const STARLINK_CATEGORY = "52";
 export const NEARBY_SATELLITE = "rest/v1/satellite/above";
