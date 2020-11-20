66036e97dacf336a7a51fad5b0ea7bc22777668c
tantanorange <44329336+tantanorange@users.noreply.github.com>
2020-11-20 10:17:20 -0500

    Add basic form box and submit button.

--- starlink/src/components/SatSetting.js
+++ starlink/src/components/SatSetting.js
@@ -17,12 +17,12 @@ class SatSettingForm extends Component {
 
         return (
             <Form {...formItemLayout} className="sat-setting" onSubmit={this.showSatellite}>
-                <Form.Item label="Longitude(degrees)">
+                <Form.Item label={"Longitude(degrees)"}>
                     {
                         getFieldDecorator("longitude", {
                             rules: [{
                                 required: true,
-                                message: "Please input your Longitude"
+                                message: "You have to input a longitude"
                             }]
                         })(<InputNumber min={-180} max={180} syle={{width: "100%"}}
                                        placeholder={"Please input Longitude"} />)
@@ -30,11 +30,72 @@ class SatSettingForm extends Component {
                     }
                 </Form.Item>
 
+                <Form.Item label={"Latitude(degrees)"}>
+                    {
+                        getFieldDecorator("latitude", {
+                            rules: [{
+                                required: true,
+                                message: "You have to input a latitude"
+                            }]
+                            })(<InputNumber min={-90} max={90} syle={{width: "100%"}}
+                                            placeholder={"Please input Latitude"}  />)
+                    }
+                </Form.Item>
 
+                <Form.Item label={"Elevation(meters)"}>
+                    {
+                        getFieldDecorator("elevation", {
+                            rules: [{
+                                required: true,
+                                message: "You have to input a elevation"
+                            }]
+                            })(<InputNumber min={-413} max={8850} syle={{width: "100%"}}
+                                            placeholder={"Please input Elevation"}  />)
+                    }
+                </Form.Item>
 
+                <Form.Item label={"Altitude(degrees)"}>
+                    {
+                        getFieldDecorator("altitude", {
+                            rules: [{
+                                required: true,
+                                message: "You have to input an altitude"
+                            }]
+                            })(<InputNumber min={0} max={90} syle={{width: "100%"}}
+                                            placeholder={"Please input Altitude"}  />)
+                    }
+                </Form.Item>
+
+                <Form.Item label={"Duration(seconds)"}>
+                    {
+                        getFieldDecorator("duration", {
+                            rules: [{
+                                required: true,
+                                message: "You have to input a duration"
+                            }]
+                            })(<InputNumber min={0} max={90} syle={{width: "100%"}}
+                                            placeholder={"Please input Latitude"}  />)
+                    }
+                </Form.Item>
+
+                <Form.Item classname={"show-nearby"}>
+                    <Button type="primary" htmlType="submit" style={{textAlign: "center"}}>
+                        Find Nearby Satellites
+                    </Button>
+
+                </Form.Item>
 
             </Form>
         );
+    }
+
+    showSatellite = e => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                this.props.onShow(values);
+            }
+        });
     }
 }
 
